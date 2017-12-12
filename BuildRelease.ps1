40f145fbec83e4c3df98108b2c4c91785bdb5448
Eirik Årdal <eaardal@outlook.com>
2017-12-12 22:33:20 +0100

    Build script

--- BuildRelease.ps1
+++ BuildRelease.ps1
@@ -1,7 +1,18 @@
 param (
 	[Parameter(Mandatory = $true)][string] $Version,
+	[Parameter(Mandatory = $true)][string] $GitBranch,
 	[Parameter(Mandatory = $true)][string] $GitTagMessage
 )
+
+if (git status --porcelain | Where {$_ -match '^\sM'})
+{
+	Write-Host "Git has changes. Commit them first and run this script again" -ForegroundColor Red
+	return
+}
+else
+{
+	Write-Host "Git is clean" -ForegroundColor Green
+}
 
 $SourceDirectory = "$pwd\Source"
 $PublishDirectory = "$SourceDirectory\Toffee.ConsoleClient\bin\Release\netcoreapp2.0\win10-x64\publish"
@@ -47,21 +58,10 @@ foreach ($ProjectFile in $ProjectFiles)
 		}
 	}
 
-	#[System.IO.File]::WriteAllLines($ProjectFile, $LinesCopy)
+	[System.IO.File]::WriteAllLines($ProjectFile, $LinesCopy)
 
-	#Write-Host "Bumped version of $ProjectFile to $Version" -ForegroundColor Green
+	Write-Host "Bumped version of $ProjectFile to $Version" -ForegroundColor Green
 }
-
-if (git status --porcelain | Where {$_ -match '^\sM'})
-{
-	Write-Host "dirty"
-}
-else
-{
-	Write-Host "clean"
-}
-
-return
 
 dotnet publish .\Source\Toffee.sln -c Release -f netcoreapp2.0 -r win10-x64 --self-contained
 
@@ -72,7 +72,7 @@ Add-Type -AssemblyName "System.IO.Compression.Filesystem"
 
 Write-Host "Created $ZipFilePath" -ForegroundColor Green
 
-#git tag -a "v$Version" -m "$GitTagMessage"
-#git tag --a
-#git commit -m "Bumped version of csprojs to v$Version"
-#git tag -n
+git add --a
+git commit -m "Bumped version of csprojs to v$Version"
+git tag -a "v$Version" -m "$GitTagMessage"
+git push origin $GitBranch --follow-tags
